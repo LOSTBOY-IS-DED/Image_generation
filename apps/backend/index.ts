a49c3ad24d15d1371fae5f5e49c14f9b70ef7c7c
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-03-12 01:05:07 +0530

    made the backend routes and checked

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,4 +1,3 @@
-import { EyeColorEnum } from './../../node_modules/.prisma/client/index.d';
 import express from "express";
 import {TrainModel , GenerateImage , GenerateImageFromPack} from "common/types";
 import { prismaClient } from "db";
@@ -9,6 +8,8 @@ const PORT = process.env.PORT || 8080 ;
 
 const app = express();
 app.use(express.json())
+
+// For training a model 
 
 app.post("/ai/training",async (req , res ) => {
     const parsedBody = TrainModel.safeParse(req.body);
@@ -36,6 +37,8 @@ app.post("/ai/training",async (req , res ) => {
     })
 });
 
+// For generating a single image
+
 app.post("/ai/generate", async (req , res ) => {
     const parsedBody = GenerateImage.safeParse(req.body)
 
@@ -47,9 +50,9 @@ app.post("/ai/generate", async (req , res ) => {
     }
     const data = await prismaClient.outputImages.create({
         data : {
-            prompt : parsedBody.data?.prompt , 
+            prompt : parsedBody.data.prompt , 
             userId : USER_ID , 
-            modelId : parsedBody.data?.modelId , 
+            modelId : parsedBody.data.modelId , 
             imageUrl : ""
         }
     })
@@ -58,18 +61,76 @@ app.post("/ai/generate", async (req , res ) => {
     })
 });
 
-app.post("/pack/generate", (req , res ) => {
+// Generating a bunch of image
+
+app.post("/pack/generate", async (req , res ) => {
+    const parsedBody = GenerateImageFromPack.safeParse(req.body);
+
+    if(!parsedBody.success){
+        res.status(411).json({
+            message : "Input incorrect"
+        }); 
+        return ; 
+    }
+    const prompt = await prismaClient.packPrompts.findMany({
+        where : {
+            packId : parsedBody.data.packId
+        }
+    })
+
+    const images = await prismaClient.outputImages.createManyAndReturn({
+        data : prompt.map(prompt => {
+            return {
+                prompt : prompt.prompt , 
+                userId : USER_ID , 
+                modelId : parsedBody.data.modelId , 
+                imageUrl : ""
+            }
+        })
+    })
+    res.json({
+        images : images.map((image) => {
+            image.id
+        })
+    })
 
 });
 
-app.post("/pack/bulk", (req , res ) => {
-    
+
+
+app.get("/pack/bulk", async (req , res ) => {
+    const packs = await prismaClient.packs.findMany({
+        
+    })
+    res.json({
+        packs
+    })
 });
 
-app.get("/image", (req , res ) => {
 
+app.get("/image/bulk", async (req , res ) => {
+    const images = req.query.images as string[];
+    const limit = req.query.limit as string;
+    const offset = req.query.offset as string;
+
+    console.log("hi there");
+
+    const imagesData = await prismaClient.outputImages.findMany({
+        where : {
+            id : {
+                in : images
+            },
+            userId : USER_ID
+        }, 
+        skip : parseInt(offset) ,
+        take : parseInt(limit)
+
+   })
+   res.json({
+    images : imagesData
+   })
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 8080");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
